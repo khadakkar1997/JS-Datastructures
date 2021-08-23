1c48a0b03733274491519a5f07450167ef8b0177
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-23 18:15:29 +0530

    Create PrimeFactors.js

---
+++ Arrays/PrimeFactors.js
@@ -0,0 +1,32 @@
+/**
+ * Extend the Prime Factorization Program to store all the Prime Factors of a number n into an array and finally display the output..
+ */
+
+ const prompt = require('prompt-sync')();
+
+ let count = 0;
+ let isPrime = 0;
+ let result = [];
+ let number = prompt("Enter any number to print its prime factors: ");
+ 
+ for ( i = 2; i <= number; i++ ) {
+
+     if ( number % i == 0) {
+         isPrime = 1;
+         
+         for ( j = 2; j <= i / 2; j++) {
+
+             if ( i % j == 0) {
+                 isPrime = 0;
+                 break;
+             }
+         }
+         if ( isPrime == 1 ) {
+             result [count] = i;
+             count++;
+         }
+     }
+ }
+ if ( isPrime == 0 || isPrime == 1 ) {
+     console.log(result);
+ }
